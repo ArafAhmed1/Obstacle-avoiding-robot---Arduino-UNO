44ba5ecdd3016d905f0d6e8c7c078357fe5d6d17
Araf Ahmed <106169727+ArafAhmed1@users.noreply.github.com>
2026-02-13 16:06:06 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Obstacle-avoiding-robot---Arduino-UNO
+
+This is the code for a obstacle avoiding robot. 
+This robot can navigate around the environment using a ultrasonic sound sensor
+It can be manually controlled using bluetooth device commands (android app)
+It can be manually controlled by voice commands using bluetooth
+
---
+++ main.c
@@ -0,0 +1,41 @@
+#include <Servo.h>
+#include <AFMotor.h>
+
+#define Echo A0
+#define Trig A1
+#define motor 10
+#define SpeedL 250
+#define SpeedR 200
+#define spoint 83 // Servo center position
+
+char value;
+int distance = 0;
+int Left;
+int Right;
+int L = 0;
+int R = 0;
+int L1 = 0;
+int R1 = 0;
+
+Servo servo;
+AF_DCMotor M1(1);
+AF_DCMotor M2(2);
+AF_DCMotor M3(3);
+AF_DCMotor M4(4);
+
+void setup()
+{
+    Serial.begin(9600);
+    pinMode(Trig, OUTPUT);
+    pinMode(Echo, INPUT);
+    servo.attach(motor);
+    // Initialize motors
+    // M1.setSpeed(Speed);
+    M2.setSpeed(SpeedL);
+    M3.setSpeed(SpeedR);
+    M4.setSpeed(SpeedR);
+
+    // Center the servo
+    servo.write(spoint);
+    delay(500);
+}
